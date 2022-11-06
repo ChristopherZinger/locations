5393385ab4e2a50aefe8e5ac6eab7ef3a4b733ae
ChristopherZinger <krzysztof.zinger@gmail.com>
2022-11-06 16:34:27 +0100

    move types to separate file

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,31 @@
+export type Translations = Record<keyof typeof CountryCodeToName, string>
+
+export type Country = {
+	name: string
+	'country-code': string // number
+	'alpha-2': string
+	'alpha-3': string
+	region: string // continent
+	'region-code': string // number "013"
+	'sub-region': string
+	'sub-region-code': string
+	'intermediate-region': string
+	'intermediate-region-code': string
+}
+
+export type CountryWithTranslations = Country & {
+	translations: Translations
+}
+
+export type City = {
+	name: string
+	subcountry: string
+	country: string
+	geonameid: string
+}
+
+export type CityWithAplha2Code = City & { 'country-alpha-2': string }
+
 export enum CountryCodeToName {
 	AD = 'Andorra',
 	AE = 'United Arab Emirates',
@@ -242,4 +270,88 @@ export enum CountryCodeToName {
 	IO = 'British Virgin Islands',
 	VI = 'U.S. Virgin Islands',
 	VN = 'Vietnam'
+}
+
+export enum LanguageCodeToName {
+	'AF' = 'AFRIKAANS',
+	'AM' = 'AMHARIC',
+	'AR' = 'ARABIC',
+	'AZ' = 'AZERBAIJANI',
+	'BE' = 'BELARUSIAN',
+	'BG' = 'BULGARIAN',
+	'BN' = 'BENGALI',
+	'BO' = 'TIBETAN STANDARD',
+	'CA' = 'CATALAN',
+	'CS' = 'CZECH',
+	'CY' = 'WELSH',
+	'DA' = 'DANISH',
+	'DE' = 'GERMAN',
+	'EE' = 'EWE',
+	'EL' = 'GREEK',
+	'EN' = 'ENGLISH',
+	'EO' = 'ESPERANTO',
+	'ES' = 'SPANISH',
+	'ET' = 'ESTONIAN',
+	'EU' = 'BASQUE',
+	'FA' = 'PERSIAN',
+	'FI' = 'FINNISH',
+	'FO' = 'FAROESE',
+	'FR' = 'FRENCH',
+	'GA' = 'IRISH',
+	'GL' = 'GALICIAN',
+	'GU' = 'GUJARATI',
+	'HE' = 'HEBREW',
+	'HI' = 'HINDI',
+	'HR' = 'CROATIAN',
+	'HU' = 'HUNGARIAN',
+	'HY' = 'ARMENIAN',
+	'IA' = 'INTERLINGUA',
+	'ID' = 'INDONESIAN',
+	'IS' = 'ICELANDIC',
+	'IT' = 'ITALIAN',
+	'JA' = 'JAPANESE',
+	'KA' = 'GEORGIAN',
+	'KI' = 'KIKUYU',
+	'KM' = 'KHMER',
+	'KN' = 'KANNADA',
+	'KO' = 'KOREAN',
+	'LG' = 'GANDA',
+	'LO' = 'LAO',
+	'LT' = 'LITHUANIAN',
+	'LV' = 'LATVIAN',
+	'MK' = 'MACEDONIAN',
+	'ML' = 'MALAYALAM',
+	'MR' = 'MARATHI',
+	'MS' = 'MALAY',
+	'MT' = 'MALTESE',
+	'MY' = 'BURMESE',
+	'NB' = 'NORWEGIAN BOKMAL',
+	'NE' = 'NEPALI',
+	'NL' = 'DUTCH',
+	'NN' = 'NORWEGIAN NYNORSK',
+	'NO' = 'NORWEGIAN',
+	'OR' = 'ORIYA',
+	'PL' = 'POLISH',
+	'PT' = 'PORTUGUESE',
+	'RM' = 'ROMANSH',
+	'RO' = 'ROMANIAN',
+	'RU' = 'RUSSIAN',
+	'SE' = 'NORTHERN SAMI',
+	'SK' = 'SLOVAK',
+	'SL' = 'SLOVENE',
+	'SN' = 'SHONA',
+	'SQ' = 'ALBANIAN',
+	'SR' = 'SERBIAN',
+	'SV' = 'SWEDISH',
+	'TA' = 'TAMIL',
+	'TE' = 'TELUGU',
+	'TH' = 'THAI',
+	'TI' = 'TIGRINYA',
+	'TL' = 'TAGALOG',
+	'TR' = 'TURKISH',
+	'UK' = 'UKRAINIAN',
+	'UR' = 'URDU',
+	'VI' = 'VIETNAMESE',
+	'ZH-CN' = 'CHINESE SIMPLIFIED',
+	'ZH-TW' = 'CHINESE TRADITIONAL'
 }
